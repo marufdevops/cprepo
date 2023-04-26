e8d8b1752ebb4a5769738be69a113608a2af0b1f
Ahmed Maruf <maruf.devops@gmail.com>
2023-04-26 15:01:47 +0600

    merge two linked lists into one linked list

--- LinkedList/mergeValues.js
+++ LinkedList/mergeValues.js
@@ -5,3 +5,48 @@
  * @returns {object} newHead example: 1 -> 2 -> 4 -> null, 1 -> 3 -> 4 -> null returns 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> null
  */
 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const a = new Node(5);
+const b = new Node(7);
+const c = new Node(10);
+const d = new Node(12);
+const e = new Node(20);
+const f = new Node(28);
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+// 5 -> 7 -> 10 -> 12 -> 20 -> 28
+
+const q = new Node(6);
+const r = new Node(8);
+const s = new Node(9);
+const t = new Node(25);
+q.next = r;
+r.next = s;
+s.next = t;
+// 6 -> 8 -> 9 -> 25
+
+const mergeValues = (l1, l2) => {
+    let head = new Node(null);
+    let current = head; 
+    while (l1 && l2) {
+        if(l1.value < l2.value) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+    current.next = l1 || l2;
+    return head.next;
+}
