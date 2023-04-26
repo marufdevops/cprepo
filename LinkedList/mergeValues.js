933fd837db15f34f99729590d6b041184c66a7a7
Ahmed Maruf <maruf.devops@gmail.com>
2023-04-26 15:01:24 +0600

    merge two linked lists into one linked list

---
+++ LinkedList/mergeValues.js
@@ -0,0 +1,7 @@
+/**
+ * Merge two sorted linked lists and return it as a sorted list.
+ * @param {object} l1
+ * @param {object} l2
+ * @returns {object} newHead example: 1 -> 2 -> 4 -> null, 1 -> 3 -> 4 -> null returns 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> null
+ */
+
